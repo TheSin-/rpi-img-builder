513edd249863607a8e0c9af16a0775966a622781
Justin F. Hallett <thesin@southofheaven.org>
2016-04-26 09:04:09 -0600

    Use tabs and fix spacing

--- common.mk
+++ common.mk
@@ -1,22 +1,22 @@
 DIST ?= jessie
 DISTS ?= Debian Bluefalls
 DIST_ARCH ?= armhf
-ifeq ($(findstring armel, $(DIST_ARCH)), armel)
-  ARCH := rpi
-else ifeq ($(findstring arm64, $(DIST_ARCH)), arm64)
-  ARCH := rpi3
+ifeq ($(findstring armel,$(DIST_ARCH)),armel)
+	ARCH := rpi
+else ifeq ($(findstring arm64,$(DIST_ARCH)),arm64)
+	ARCH := rpi3
 else
-  ARCH := rpi2
+	ARCH := rpi2
 endif
 
 U-BOOT ?= false
 
-ifeq ($(findstring true, $(U-BOOT)), true)
-  ARCH := rpi2
-  DIST_ARCH := armhf
-  ifneq ($(findstring Debian, $(DISTS)), Debian)
-    DISTS += Debian
-  endif
+ifeq ($(findstring true,$(U-BOOT)),true)
+	ARCH := rpi2
+	DIST_ARCH := armhf
+	ifneq ($(findstring Debian,$(DISTS)),Debian)
+		DISTS += Debian
+	endif
 endif
 
 ROOT_DEV := /dev/mmcblk0p2
@@ -30,12 +30,12 @@ LOCALE ?= en_US.UTF-8
 IMAGE_MB ?= -1
 BOOT_MB ?= 38
 ifeq ($(shell test $(BOOT_MB) -lt 38; echo $$?),0)
-  BOOT_MB := 38
+	BOOT_MB := 38
 endif
 ifeq ($(IMAGE_MB),-1)
-  ROOT_MB := -1
+	ROOT_MB := -1
 else
-  ROOT_MB=$(shell expr $(IMAGE_MB) - $(BOOT_MB))
+	ROOT_MB=$(shell expr $(IMAGE_MB) - $(BOOT_MB))
 endif
 
 BOOT_DIR := boot
